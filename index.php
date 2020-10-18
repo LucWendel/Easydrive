86ec6670c4a66b5ab9d32020e241c565597c2b15
Corne Siebeling <2078538@talnet.nl>
2020-10-18 21:42:30 +0200

    joee

--- index.php
+++ index.php
@@ -41,7 +41,22 @@
     <h3 class="onderondertekst">
       geslaagde mensen: 002
     </h3>
+    <?php  
+    if (isset($_SESSION['username'])) : ?> 
+            <p> 
+                Welcome  
+                <strong> 
+                    <?php echo $_SESSION['username']; ?> 
+                </strong> 
+            </p> 
+            <p>  
+                <a href="index.php?logout='1'" style="color: red;"> 
+                    Click here to Logout 
+                </a> 
+            </p> 
+        <?php endif ?> 
   </footer>
+
 </body>
 
 </html>
